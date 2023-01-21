c9450d56045c9a3ed57aab6eddceeccbc7db613f
Hubert Debowski <huberthubert76@gmail.com>
2023-01-21 14:16:47 +0100

    Calculate number of walls

--- wall.py
+++ wall.py
@@ -3,3 +3,8 @@ import ifcopenshell.util.element
 
 model = ifcopenshell.open("AC-20-Smiley-West-10-Bldg.ifc")
 
+walls = model.by_type("IfcWall")
+
+print(f'Liczba ścian w modelu: {len(walls)}')
+
+
